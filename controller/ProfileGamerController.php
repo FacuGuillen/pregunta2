580b5df3a25b761ec901db25d77bb9861affe0a6
angela quelca <anahi.laurani@gmail.com>
2025-07-02 15:35:34 -0300

    el controller de ranking funciona sin el security y ademas si se quiere insertar x la url datos no validas te manda error y vuekve al ranking

--- controller/ProfileGamerController.php
+++ controller/ProfileGamerController.php
@@ -5,39 +5,42 @@ class ProfileGamerController{
     public $view;
     private $model;
 
-
     public function __construct($model,$view){
         $this->model = $model;
         $this->view = $view;
     }
 
     public function show($nombreJugador = null){
+        $username = $_SESSION["user"]["nombre_usuario"] ?? null;
 
-     //   if ($nombreJugador == null) {
-      //      header('location: /ranking/show?error=null');
-        //    exit();
-        //}
-
-        $username = $_SESSION["user"]["nameuser"] ?? null;
+        if ($nombreJugador == null) {
+            header('location: /ranking/show?error=null');
+            exit();
+        }
 
         $jugador = $this->model->traerLosdatosDelUsuarioYSuRanking($nombreJugador);
-        $data = [  "jugador" => $jugador[0]] ;
+
+        if (empty($jugador) || empty($jugador[0])) {
+            header('location: /ranking/show?error=invalido');
+            exit();
+        }
+
+        $data = ["jugador" => $jugador[0]] ;
 
         // genero qr
         $idUsuario = $data["jugador"]["id_usuario"];
-        // deberia cambiar segun la ruta en mi caso anda xq estoy usando la ruta8080
         $qr_url = "http://localhost:8080/profile/show/$idUsuario";
 
         $context = array_merge($data, [
             'username' => $username,
-            '$qr_url' =>$qr_url
+            'qr_url' =>$qr_url
         ]);
 
         $this->view->render("profileGamer", $context);
     }
 
-    public function generarQr($idUsuario)
-    {    $qr_url = "http://localhost:8080/profile/show/$idUsuario";
+    public function generarQr($idUsuario){
+        $qr_url = "http://localhost:8080/profile/show/$idUsuario";
         QRcode::png($qr_url, false, QR_ECLEVEL_L, 8);
     }
 }
